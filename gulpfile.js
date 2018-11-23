fcc4397104ed417ae2ae2cca92c24195ae857d32
AlexLavrenchuk <Alex.Lavrenchuk87@gmail.com>
2018-11-23 03:22:23 +0200

    made the second tab without adaptability and made the structure messages

--- gulpfile.js
+++ gulpfile.js
@@ -47,7 +47,7 @@ gulp.task('sprite', function () {
         .pipe(spritesmith({
             imgName: 'sprite.png',
             cssName: 'sprite.css',
-            imgPath: '../img/sprite.png'
+            imgPath: '/img/sprite.png'
         }));
 
     let imgStream = spriteData.img
